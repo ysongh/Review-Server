3a52f7afbe05270a490ab6dc78829395f727c328
You Song Hou <yousonghou@yahoo.com>
2020-02-29 10:58:33 -0500

    set up sqlite

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask_marshmallow import Marshmallow
 import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 # testing
 @app.route("/", methods=["GET"])
